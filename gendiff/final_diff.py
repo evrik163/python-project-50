7cb841e9a6b47cd5700f7641c790932eb79dac17
evrik163 <evrik163@mail.ru>
2023-03-11 19:44:45 +0400

    9

--- gendiff/final_diff.py
+++ gendiff/final_diff.py
@@ -5,7 +5,7 @@ from gendiff.parsers.plain_parser import plain_formatter
 from gendiff.parsers.json_dump import json_formatter
 
 
-def generate_diff(file_path1, file_path2, format_name):
+def generate_diff(file_path1, file_path2, format_name="stylish"):
     dic1, dic2 = parser_func(file_path1, file_path2)
     lst = travel(dic1, dic2)
     if format_name == 'stylish':
